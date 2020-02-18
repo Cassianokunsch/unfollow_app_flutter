9004bdac6e4d84b1e8d5370d795263098e36958c
Cassiano Kunsch das Neves <cassiano.neves@avista.local>
2020-02-18 17:49:09 -0300

    Ajustando melhor os componentes

--- lib/main.dart
+++ lib/main.dart
@@ -19,53 +19,34 @@ class MyApp extends StatelessWidget {
           child: Column(
             mainAxisAlignment: MainAxisAlignment.center,
             children: <Widget>[
-              Column(
-                children: <Widget>[
-                  Text(
-                    "Unfollow",
-                    style: TextStyle(
-                        color: Colors.white,
-                        fontSize: 70,
-                        fontFamily: 'Lobster'),
-                  ),
-                  Text(
-                    "App",
-                    style: TextStyle(
-                        color: Colors.white,
-                        fontSize: 70,
-                        fontFamily: 'Lobster'),
-                  ),
-                ],
+              buildTitleLogin(),
+              SizedBox(
+                height: 60.0,
               ),
-              Container(
-                padding: EdgeInsets.only(left: 30, right: 30),
-                margin: EdgeInsets.only(top: 30),
+              Padding(
+                padding: const EdgeInsets.only(left: 30.0, right: 30.0),
                 child: Column(
+                  crossAxisAlignment: CrossAxisAlignment.stretch,
                   children: <Widget>[
-                    TextField(
-                      decoration: InputDecoration(
-                          border: OutlineInputBorder(
-                            borderRadius: BorderRadius.all(
-                              Radius.circular(5),
-                            ),
-                          ),
-                          filled: true,
-                          hintStyle: TextStyle(color: Colors.grey[800]),
-                          hintText: "Username",
-                          fillColor: Color.fromRGBO(255, 255, 255, 0.5)),
+                    buildTextField("Username", false),
+                    SizedBox(
+                      height: 10.0,
                     ),
-                    TextField(
-                      obscureText: true,
-                      decoration: InputDecoration(
-                          border: OutlineInputBorder(
-                            borderRadius: BorderRadius.all(
-                              Radius.circular(5),
-                            ),
-                          ),
-                          filled: true,
-                          hintStyle: TextStyle(color: Colors.grey[800]),
-                          hintText: "Password",
-                          fillColor: Color.fromRGBO(255, 255, 255, 0.3)),
+                    buildTextField("Password", true),
+                    SizedBox(
+                      height: 20.0,
+                    ),
+                    ButtonTheme(
+                      height: 54.0,
+                      child: RaisedButton(
+                        color: Colors.blue,
+                        textColor: Colors.white,
+                        disabledColor: Colors.grey,
+                        disabledTextColor: Colors.black,
+                        splashColor: Colors.blueAccent,
+                        onPressed: () {},
+                        child: Text("Entrar", style: TextStyle(fontSize: 20.0)),
+                      ),
                     ),
                   ],
                 ),
@@ -74,6 +55,44 @@ class MyApp extends StatelessWidget {
           ),
         ),
       ),
+    );
+  }
+
+  TextField buildTextField(hintText, obscureText) {
+    return TextField(
+      cursorColor: Color.fromRGBO(255, 255, 255, 0.5),
+      obscureText: obscureText,
+      decoration: InputDecoration(
+          focusedBorder: OutlineInputBorder(
+            borderSide: BorderSide(
+                color: Color.fromRGBO(255, 255, 255, 0.3), width: 2.0),
+          ),
+          border: OutlineInputBorder(
+            borderRadius: BorderRadius.all(
+              Radius.circular(5),
+            ),
+          ),
+          filled: true,
+          hintStyle: TextStyle(color: Colors.grey[800]),
+          hintText: hintText,
+          fillColor: Color.fromRGBO(255, 255, 255, 0.5)),
+    );
+  }
+
+  Column buildTitleLogin() {
+    return Column(
+      children: <Widget>[
+        Text(
+          "Unfollow",
+          style: TextStyle(
+              color: Colors.white, fontSize: 70, fontFamily: 'Lobster'),
+        ),
+        Text(
+          "App",
+          style: TextStyle(
+              color: Colors.white, fontSize: 70, fontFamily: 'Lobster'),
+        ),
+      ],
     );
   }
 }
