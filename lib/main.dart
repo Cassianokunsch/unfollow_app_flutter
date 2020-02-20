6893b50d1f009caeeea61d524fafcedbdef88c4a
Cassiano Kunsch das Neves <cassiano.neves@avista.local>
2020-02-20 18:49:24 -0300

    Refactor

--- lib/main.dart
+++ lib/main.dart
@@ -7,12 +7,10 @@ void main() => runApp(MyApp());
 class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      home: ClientProvider(
-        // uri: "https://unfollow-app-graphene.herokuapp.com/",
-        uri: "http://0.0.0.0:5000/",
-        child: MaterialApp(routes: routes),
-      ),
+    return ClientProvider(
+      // uri: "https://unfollow-app-graphene.herokuapp.com/",
+      uri: "http://10.15.240.14:5000/",
+      child: MaterialApp(routes: routes),
     );
   }
 }
