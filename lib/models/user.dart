2286c645343ad352d718989100e35e15cc8990f7
Cassiano Kunsch das Neves <cassiano.neves@avista.local>
2020-02-21 18:46:49 -0300

    create model

---
+++ lib/models/user.dart
@@ -0,0 +1,29 @@
+class User {
+  String pk;
+  String username;
+  String fullName;
+  bool isPrivate;
+  String profilePicUrl;
+  String profilePicId;
+  bool isVerified;
+  bool hasAnonymousProfilePicture;
+  int latestReelMedia;
+  int followerCount;
+  int followingCount;
+  String biography;
+
+  User.fromJson(Map<String, dynamic> json) {
+    this.biography = json['biography'];
+    this.followerCount = json['followerCount'];
+    this.followingCount = json['followingCount'];
+    this.fullName = json['fullName'];
+    this.hasAnonymousProfilePicture = json['hasAnonymousProfilePicture'];
+    this.isPrivate = json['isPrivate'];
+    this.isVerified = json['isVerified'];
+    this.latestReelMedia = json['latestReelMedia'];
+    this.pk = json['pk'];
+    this.profilePicId = json['profilePicId'];
+    this.profilePicUrl = json['profilePicUrl'];
+    this.username = json['username'];
+  }
+}
